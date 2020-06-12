d9955da634282c9dfb87ae6a9ef42ce7c21eaeb1
Nick <naf.integra@gmail.com>
2020-06-11 23:15:37 -0500

    update

--- nfintegra_jscript.js
+++ nfintegra_jscript.js
@@ -2,18 +2,25 @@ document.addEventListener('DOMContentLoaded',
   ()=> anime.timeline({
 
   })
+
   .add({
     targets: '#bgcirc',
     scale: ['6','1'],
     easing: 'spring(2, 20, 12, 0)',
-    duration: 50000,
-  })
-  
+    duration: '50000',
+  }) 
   .add({
-    targets: '#bgcirc',
-    scale: ['6','1'],
-    easing: 'spring(2, 20, 12, 0)',
-    duration: 50000,
+    targets: '.fillcir',
+    fill: ['rgba(255,255,255,0)', '#ffffff'],
+    easing: 'linear',
+    duration: '250',
+  }) 
+  .add({
+    targets: '.strokecir',
+    fill: ['rgba(255,255,255,0)', '#ffffff'],
+    stroke: ['rgba(0,0,0,0)', '#000000'],
+    easing: 'linear',
+    duration: '1200',
   }) );
 
 /*let path = anime.path('#mpath path');
