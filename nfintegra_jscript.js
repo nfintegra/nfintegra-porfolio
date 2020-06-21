70ea0d3a97b77d603dc237146d434930f748b339
Nick <naf.integra@gmail.com>
2020-06-21 01:24:26 -0500

    update

--- nfintegra_jscript.js
+++ nfintegra_jscript.js
@@ -8,11 +8,19 @@ document.addEventListener('DOMContentLoaded',
 
   })
   .add({
-    targets: '#fillcir1 #strokecir',
+    targets: '#fillcir1, #fillcir2, #fillcir3',
     fill: ['rgba(255,255,255,0)', '#ffffff'],
     easing: 'linear',
-    duration: '250',
+    duration: '4250',
   }) 
+
+  .add({
+    targets: '#strokecir1, #strokecir2, #strokecir3',
+    fill: ['rgba(255,255,255,0)', '#ffffff'],
+    stroke: ['rgba(0,0,0,0)', '#000000'],
+    easing: 'linear',
+    duration: '3200',
+  },'-=200')
 
   .add({
     targets: '#covercirc',
@@ -41,14 +49,8 @@ document.addEventListener('DOMContentLoaded',
     easing: 'linear',
     duration: '5000',
   }, '-=2500')
-
-  .add({
-    targets: '.strokecir',
-    fill: ['rgba(255,255,255,0)', '#ffffff'],
-    stroke: ['rgba(0,0,0,0)', '#000000'],
-    easing: 'linear',
-    duration: '1200',
-  },'-=200') );
+  
+);
 
 /*
 let path = anime.path('#mpath path');
@@ -62,5 +64,3 @@ anime({
   loop: true
 });
 */
-
-
