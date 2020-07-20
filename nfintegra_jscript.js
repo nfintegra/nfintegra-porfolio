e1653c893ff9429fb5d9069e6a91615bacbcd67e
Nick <naf.integra@gmail.com>
2020-07-19 23:13:40 -0500

    UPDATE BITCHls

--- nfintegra_jscript.js
+++ nfintegra_jscript.js
@@ -2,6 +2,7 @@ const covercirc = document.querySelector('svg#logo g#logo_grp path#covercirc');
 const stroke1 = document.querySelector('svg#logo g#logo_grp path#cstroke1.cover_stroke');
 const stroke2 = document.querySelector('svg#logo g#logo_grp path#cstroke2.cover_stroke');
 const stroke3 = document.querySelector('svg#logo g#logo_grp line#lstroke3');
+const projectbtn = document.querySelector('svg#logo g#logo_grp g#strokegrp circle#projectbtn')
 
 const fillanimation = {
   fill: ['rgba(255,255,255,0)', '#ffffff'],
@@ -55,6 +56,161 @@ document.addEventListener('DOMContentLoaded',
   }, '-=3000')
 )
 
+var rotationloopcir = anime({
+  targets: '.rotatecir',
+  rotate: {
+      value: 360,
+      duration: 10000,
+      easing: 'linear'
+  },
+  loop: true
+}, 400)
+
+projectbtn.addEventListener('mouseenter', () => {
+  var btnScaleUp = anime({
+    targets: '#projectbtn',
+    strokeWidth: ['.75', '.25'],
+    fill: ['#ffffff', '#ff0000'],
+    easing: 'linear',
+    duration: '250',
+    autoplay: false,
+  })
+
+  btnScaleUp.play();
+  rotationloopcir.pause();
+
+})
+
+projectbtn.addEventListener('mouseleave', () => {
+  var btnScaleDown = anime({
+    targets: '#projectbtn',
+    strokeWidth: ['.75', '.25'],
+    fill: ['#ffffff', '#ff0000'],
+    easing: 'linear',
+    duration: '250',
+    direction: 'reverse',
+    autoplay: false,
+  })
+
+  btnScaleDown.play();
+  rotationloopcir.play();
+})
+
+
+
+
+// rotateCircle();
+
+
+/*
+
+var rotationloopcir = anime({
+    targets: '.rotatecir',
+    rotate: {
+        value: 360,
+        duration: 10000,
+        easing: 'linear'
+    },
+    loop: true
+  }, 400)
+  
+projectbtn.addEventListener('mouseenter', () => {
+  btnScaleUp.play();
+  rotationloopcir.pause();
+})
+
+projectbtn.addEventListener('mouseleave', () => {
+  btnScaleUp.reverse();
+  rotationloopcir.play();
+});
+
+mouseIn();
+mouseOut();
+
+var mouse = {
+  x: undefined,
+  y: undefined
+}
+
+window.addEventListener('mousemove',  
+    function(event){
+      mouse.x = event.x;
+      mouse.y = event.y;
+})
+
+function rotateCircle() { 
+  anime({
+    targets: '.rotatecir',
+    rotate: {
+        value: 360,
+        duration: 10000,
+        easing: 'linear'
+    },
+    loop: true
+  }, 400)
+};
+
+function resumeBtnOverEvent() {
+  var btnoverevt = false;
+  var btnoverevt = projectbtn.addEventListener('mouseover', () => {
+    btnScaleUp.play;
+
+      if (btnoverevt == true){
+        rotateCirclecxa.pause();
+      } 
+  }) 
+
+  var btnoutevt = projectbtn.addEventListener('mouseout', () => {
+    btnScaleUp.reverse();
+      
+    if (btnoutevt == true){
+      rotateCircle();
+    } 
+  })
+};
+
+resumeBtnEvent();
+
+projectbtn.addEventListener('mouseover', () => {
+  btnScaleUp.play();
+})
+
+let btnScaleDwn = btnScaleUp.reverse();
+
+projectbtn.addEventListener('mouseout', () => {
+  btnScaleDwn
+})
+
+
+
+projectbtn.addEventListener('mouseout', () => {
+  btnScaleUp.reverse();
+})
+
+/*
+  btnScaleUp.reverse();
+
+function setUpEvents (){
+    
+  test.addEventlistener('click', buttonEnlarge);
+  
+  function buttonEnlarge() {
+    anime({
+      targets: '#projectBtn',
+      translateX: 0,
+      translateY: 11,
+      scale: (1.8),
+      stroke: ['.25', '0'],
+      fill: ['#ffffff', '#ff0000'],
+      autoplay: false,
+    }) 
+  };
+} 
+
+window.onload = function(){
+setUpEvents();
+}
+/*
 anime({
   targets: '.rotatecir',
   rotate: {
@@ -63,9 +219,12 @@ anime({
       easing: 'linear'
   },
   loop: true
-})
+}, 400)
+
+projectbtn.addEventlistener('click', btnenlrg);
 
 /*
+
 let path = anime.path('#mpath path');
 
 anime({
